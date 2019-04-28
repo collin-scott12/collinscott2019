1d7a4dac1f72d5b78cde20322d0d5acd680ffed0
Collin Scott <mrmediadesigns@gmail.com>
2019-04-28 18:48:35 -0400

    Fixed Client Folder again

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/style.sass'
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p class="footer-text">Copyright © 2019  Collin Scott</p>
+            </footer>
+        )
+    }
+}
+
+export default Footer;
