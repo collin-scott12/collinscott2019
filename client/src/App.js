e2210206e287c3b2e48acb44f05a2b5bedacc541
Collin Scott <mrmediadesigns@gmail.com>
2019-05-04 14:17:44 -0400

    Removed error

--- client/src/App.js
+++ client/src/App.js
@@ -11,18 +11,18 @@ import Resume from "./pages/Resume";
 import { normalize } from "path";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
-  }
-  componentWillMount() {
-    this.callAPI();
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { apiResponse: "" };
+  // }
+  // callAPI() {
+  //   fetch("http://localhost:9000/testAPI")
+  //     .then(res => res.text())
+  //     .then(res => this.setState({ apiResponse: res }));
+  // }
+  // componentWillMount() {
+  //   this.callAPI();
+  // }
   render() {
     return (
       // <p className="App-intro">{this.state.apiResponse}</p>
